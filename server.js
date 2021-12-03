08cd7f619816987892d7e5a5ee913db0f2a89c54
Tejas Ravuri <tsravuri@gmail.com>
2021-12-02 23:46:25 -0500

    msg add

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.delete("/app/delete/user/:id", (req, res) => {
 });
 // Default response for any other request
 app.use(function(req, res){
-	res.json({"message":"Endpoint not found. (404)"});
+	res.json({"message":"Your API is working!"});
     res.status(404);
 });
